742dfc478e988b13a410aba6ec12102e7f03d338
Devine Lu Linvega <aliceffekt@gmail.com>
2018-05-06 09:15:52 +1200

    Note can be hidden

--- scripts/note.js
+++ scripts/note.js
@@ -1,11 +1,19 @@
 function Note(client)
 {
   this.el = document.getElementById("note");
+  this.el.className = "hidden"
+  this.el.addEventListener("click", ()=>{ client.note.hide() });
   this.client = client;
 
   this.update = function(target,y)
   {
+    this.el.className = ""
     this.el.style.top = `${y}px`
     this.el.innerHTML = this.client.lexicon[target.toUpperCase()] ? new Runic(this.client.lexicon[target.toUpperCase()]).toString() : `<p>Unknown ${target} item.</p>`
+  }
+  
+  this.hide = function()
+  {
+    this.el.className = "hidden"
   }
 }
