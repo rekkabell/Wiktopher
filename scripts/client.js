6d6ab66f7835ee8ec64ab1de55f07a85f8836048
Devine Lu Linvega <aliceffekt@gmail.com>
2018-07-07 14:24:03 +1200

    Improved linking

--- scripts/client.js
+++ scripts/client.js
@@ -11,15 +11,19 @@ function Client(story,lexicon)
 
   this.start = function()
   {
-    console.log("Starting client..")
-    this.load()
+    var target = window.location.hash.replace(/\+/g," ").replace("#","").toUpperCase().trim()
+
+    this.load(target)
   }
 
   this.load = function(target = Object.keys(this.story)[0])
   {
     if(target == "lexicon"){ this.show_lexicon(); return; }
-    if(!this.story[target]){ return; }
+    if(target == "HOME"){ target = Object.keys(this.story)[0] }
+    if(!this.story[target]){ console.warn("Error",target); return; }
 
+    window.location.hash = target.to_url()
+    document.title = `Wiktopher — ${target.capitalize()}`
     this.index = target
 
     this.navi.update();
@@ -36,7 +40,6 @@ function Client(story,lexicon)
 
   this.show_lexicon = function()
   {
-
     var html = ""
     for(id in this.lexicon){
       var segment = this.lexicon[id];
@@ -48,4 +51,14 @@ function Client(story,lexicon)
   }
 
   window.onclick = (el)=>{ this.click(el) };
+}
+
+String.prototype.capitalize = function()
+{
+  return this.charAt(0).toUpperCase() + this.slice(1).toLowerCase();
+}
+
+String.prototype.to_url = function()
+{
+  return this.toLowerCase().replace(/ /g,"+").replace(/[^0-9a-z\+\:\-]/gi,"").trim();
 }
--- scripts/navi.js
+++ scripts/navi.js
@@ -5,10 +5,11 @@ function Navi(client)
 
   this.update = function()
   {
+    console.log()
     var html = ""
     for(id in this.client.story){
       var segment = this.client.story[id];
-      html += `<ln><a onclick='client.load("${id}")'>${id.capitalize()}</a></ln>`
+      html += `<ln class='chapter ${this.client.index == id ? 'selected' : ''}'><a onclick='client.load("${id}")'>${id.capitalize()}</a></ln>`
     }
     html += `<ln><a onclick='client.load("lexicon")'>Lexicon</a></ln>`
     this.el.innerHTML = `<list>${html}</list>`;
